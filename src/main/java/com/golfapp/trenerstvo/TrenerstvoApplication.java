f7793407764ec16768be956216acdb4f1a532d11
willy161 <gasper.jezovsek@gmail.com>
2025-11-20 18:50:05 +0100

    maven structure

---
+++ src/main/java/com/golfapp/trenerstvo/TrenerstvoApplication.java
@@ -0,0 +1,13 @@
+package com.golfapp.trenerstvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrenerstvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrenerstvoApplication.class, args);
+	}
+
+}
