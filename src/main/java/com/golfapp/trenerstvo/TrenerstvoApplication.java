4f7d3df25dd44b42f6a1fcdd4dcfa88118d48504
vidc101 <vc7492@student.uni-lj.si>
2026-01-08 18:08:47 +0100

    Koncana glavna funkcionalnost mikroservica

--- src/main/java/com/golfapp/trenerstvo/TrenerstvoApplication.java
+++ src/main/java/com/golfapp/trenerstvo/TrenerstvoApplication.java
@@ -2,8 +2,10 @@ package com.golfapp.trenerstvo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.golfapp.trenerstvo.model")
 public class TrenerstvoApplication {
 
     public static void main(String[] args) {
