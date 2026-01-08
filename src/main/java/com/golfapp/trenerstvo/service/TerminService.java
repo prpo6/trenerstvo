771d491a6bd6f5d0785c9dfc1b6066e5c296de80
vidc101 <vc7492@student.uni-lj.si>
2026-01-08 12:23:46 +0100

    Zacetek servicov

--- src/main/java/com/golfapp/trenerstvo/service/TerminService.java
+++ src/main/java/com/golfapp/trenerstvo/service/TerminService.java
@@ -1,5 +1,8 @@
 package com.golfapp.trenerstvo.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TerminService {
-    
+
 }
--- src/main/java/com/golfapp/trenerstvo/service/TrenerService.java
+++ src/main/java/com/golfapp/trenerstvo/service/TrenerService.java
@@ -1,5 +1,18 @@
 package com.golfapp.trenerstvo.service;
 
+import com.golfapp.trenerstvo.model.Trener;
+import com.golfapp.trenerstvo.repository.TrenerRepository;
+
+import java.util.List;
+
 public class TrenerService {
-    
+    private final TrenerRepository trenerRepository;
+
+    public TrenerService(TrenerRepository trenerRepository) {
+        this.trenerRepository = trenerRepository;
+    }
+
+    public List<Trener> getAll() {
+        return trenerRepository.findAll();
+    }
 }
